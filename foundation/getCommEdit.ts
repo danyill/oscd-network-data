5455a88c3d01166d4d1ab289fff84e1ab048b16b
Daniel Mulholland <dan.mulholland@gmail.com>
2023-11-07 14:23:56 +1300

    Improve apName on subscription information, part of #2

--- foundation/getCommEdit.ts
+++ foundation/getCommEdit.ts
@@ -2,16 +2,38 @@ import { getReference } from '@openenergytools/scl-lib';
 import { createElement } from '@openenergytools/scl-lib/dist/foundation/utils';
 import { Insert } from '@openscd/open-scd-core';
 
+/** @returns the cartesian product of `arrays` */
+export function crossProduct<T>(...arrays: T[][]): T[][] {
+  return arrays.reduce<T[][]>(
+    (a, b) => <T[][]>a.flatMap(d => b.map(e => [d, e].flat())),
+    [[]]
+  );
+}
+
 export function getCommEdit(
   address: Element,
   privateSCL: Element,
   iedName: string
 ): Insert | undefined {
-  // const apIedNameRx = address.closest('ConnectedAP')!.getAttribute('iedName');
-  const apName = address.closest('ConnectedAP')!.getAttribute('apName');
+  const doc = address.ownerDocument;
 
   const subNetwork = address.closest('SubNetwork')!;
-  const doc = address.ownerDocument;
+
+  const cbName = address.getAttribute('cbName');
+
+  // The apName is associated with the AccessPoint of the subscribing ExtRef
+  // but this is not easy to determine if there are ServerAt directives and
+  // the Services section
+  const apName =
+    doc
+      .querySelector(
+        `:root > IED[name="${iedName}"] > AccessPoint > Server > LDevice > LN0 > Inputs > ExtRef[srcCBName="${cbName}"], 
+                     :root > IED[name="${iedName}"] > AccessPoint > Server > LDevice > LN > Inputs > ExtRef[srcCBName="${cbName}"]`
+      )
+      ?.closest('AccessPoint')
+      ?.getAttribute('name') ?? 'No Access Point Found';
+
+  // const otherAPs = Array.from(doc.querySelectorAll(`:root > IED[name="${iedName}"] > AccessPoint > ServerAt[apName="${apName}"]`)).map(serverAt => serverAt.closest('AccessPoint')?.getAttribute('name'))
 
   const connectedAp = subNetwork.querySelector(
     `ConnectedAP[iedName="${iedName}"][apName="${apName}"]`
